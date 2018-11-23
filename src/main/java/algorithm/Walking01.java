15dd634bff0d6819f8c414741b3a6473aef32283
Anton Lukyanau <lukyanov.box@gmail.com>
2018-11-24 02:10:30 +0300

    walking 0/1 light

--- src/main/java/algorithm/Walking01.java
+++ src/main/java/algorithm/Walking01.java
@@ -72,4 +72,45 @@ public class Walking01 {
       catch (MemoryBusinessException ignored) {
       }
    }
+
+   public static void runLight(Element[][] testableMemory) {
+      Execution execution = new Execution();
+      try {
+         long startTime = System.currentTimeMillis();
+
+         execution.fromLowToHigh(testableMemory, of(WRITE_0));
+
+         for (int i = 0; i < testableMemory.length; i++) {
+            for (int j = 0; j < testableMemory[i].length; j++) {
+               Element baseEl = testableMemory[i][j];
+               baseEl.set(true);
+               for (int k = 0; k < testableMemory.length; k++) {
+                  for (int l = 0; l < testableMemory[k].length; l++) {
+                     Element innerel = testableMemory[k][l];
+                     if (innerel != baseEl & innerel.get()) {
+                        System.out.println(String.format("Error in  cell [%d][%d]", k, l));
+                        throw new MemoryBusinessException();
+                     }
+                  }
+               }
+               if (!baseEl.get()) {
+                  System.out.println(String.format("Error in  cell [%d][%d]", i, j));
+                  throw new MemoryBusinessException();
+               }
+               baseEl.set(false);
+            }
+         }
+
+
+         long finishTime = System.currentTimeMillis();
+
+         int size = testableMemory.length * testableMemory[0].length / 1000000;
+         System.out.println(
+               String.format("Walking 0/1 LIGHT. Check performed. size: %d Mbit | Processing time: %d ms | Found 0 errors",
+                     size,
+                     finishTime - startTime));
+      }
+      catch (MemoryBusinessException ignored) {
+      }
+   }
 }
